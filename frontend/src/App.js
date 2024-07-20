c23a1c0d05a702ffcdf6cbb4d180be4cc89cf3a1
jasmeen <jasmeenbano12@gmail.com>
2024-07-20 18:09:48 +0530

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UsePagination from './Pagination';
+
+function App() {
+  return (
+    <div className="App">
+      <UsePagination/>
+    </div>
+  );
+}
+
+export default App;
